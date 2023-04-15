69815d1013824afb5d16198cd3b479f7511d083c
Rudraa332 <rudraa2213@proton.mail>
2023-04-15 21:10:55 +0530

    Hello Bhai

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    BOT_TOKEN = ""
+    API_ID = ""
+    API_HASH = ""
+    AUTH_USERS = ''
+
