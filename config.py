fca4115e2923029351568a35aaef0e8ff66fc238
Vinisha <110597375+kd2929@users.noreply.github.com>
2023-05-03 17:08:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = ""
-    API_ID = ""
-    API_HASH = ""
-    AUTH_USERS = ''
+    BOT_TOKEN = "6047785902:AAGT-_mx4Gx-GK3zUshtrqmbjUPxoGx0NF8"
+    API_ID = "24250238"
+    API_HASH = "cb3f118ce5553dc140127647edcf3720"
+    AUTH_USERS = '6175650047'
 
